9824c24070c34dbec55d0dba211e25a44492ef61
章小慢 <manyangz@jumei.com>
2017-11-20 18:58:02 +0800

    done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sharding.alfredworkflow
+
+## demo
+
+```
+¢ php main.php 123456
+{"items":[{"arg":"2_9_72","autocomplete":"123456","icon":{"path":"icon.png"},"subtitle":"2_9_72","title":"sharding ","type":"default","uid":"1","valid":true}]}
+```
+
+```
+¢ php main.php zhangxiaoman
+{"items":[{"arg":"3_13_105","autocomplete":"zhangxiaoman","icon":{"path":"icon.png"},"subtitle":"3_13_105","title":"sharding ","type":"default","uid":"1","valid":true}]}
+
+```
+
--- main.php
+++ main.php
@@ -1,7 +1,33 @@
 <?php
 /**
- * User: zhangxiaoman.
- *
- * @author 章小慢<manyangz@jumei.com>
+ * @author 章小慢
  * @date 2017/11/20 18:27
  */
+
+require "vendor/autoload.php";
+
+unset($argv[0]);
+$str = implode(" ", $argv);
+$cfg = "";
+if (ctype_digit($str)) {
+    $last3 = substr($str, -3);
+    $cfg = floor($last3 % 128 / 8 / 4);
+} else {
+    $last3 = substr((crc32(strtolower($str))), -3);
+    $cfg = floor($last3 % 128 / 8 / 4);
+}
+$result = $cfg."_".floor($last3 % 128 / 8)."_".$last3 % 128;
+
+$workflow = new \Alfred\Workflows\Workflow();
+
+$workflow->result()
+    ->uid('1')
+    ->title('sharding ')
+    ->subtitle($result)
+    ->type('default')
+    ->arg($result)
+    ->valid(true)
+    ->icon('icon.png')
+    ->autocomplete($str);
+
+echo $workflow->output();
